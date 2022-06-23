db843a2d7a3bd3329114798959cde09b9cc98d99
David Viloria Ortega <david.viloria@citi.com.mx>
2022-06-23 12:23:51 -0500

    Configuracion inicial de proyecto

---
+++ SwiftLib_DavidVo/SwiftLib_DavidVo.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLib_DavidVo.h
+//  SwiftLib_DavidVo
+//
+//  Created by David Viloria Ortega on 23/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLib_DavidVo.
+FOUNDATION_EXPORT double SwiftLib_DavidVoVersionNumber;
+
+//! Project version string for SwiftLib_DavidVo.
+FOUNDATION_EXPORT const unsigned char SwiftLib_DavidVoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib_DavidVo/PublicHeader.h>
+
+
